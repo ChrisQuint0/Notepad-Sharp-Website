eddb39854cf9a67055ae344155c51c0bc17ac4c2
Christopher Quinto <cquinto.primary@gmail.com>
2026-02-09 14:51:33 +0800

    Release Version 0.2.3 - XSL Support
    Signed-off-by: Christopher Quinto <cquinto.primary@gmail.com>

--- script.js
+++ script.js
@@ -54,13 +54,13 @@ document.addEventListener("DOMContentLoaded", () => {
   if (installerButton) {
     installerButton.addEventListener("click", () => {
       window.location.href =
-        "installer_stand_alone/Notepad Sharp_0.2.2_x64_en-US.msi";
+        "installer_stand_alone/Notepad Sharp_0.2.3_x64_en-US.msi";
     });
   }
 
   if (standaloneButton) {
     standaloneButton.addEventListener("click", () => {
-      window.location.href = "installer_stand_alone/Notepad Sharp_0.2.2.exe";
+      window.location.href = "installer_stand_alone/Notepad Sharp_0.2.3.exe";
     });
   }
 });
